739c826cbe029c1b9781cc79e5a439c5dd97e393
Rishi Mukherjee <thethunder666@gmail.com>
2013-03-10 23:08:16 +0530

    Added drop down for dimension and argument type.

--- scripts/param.js
+++ scripts/param.js
@@ -3,8 +3,8 @@ function validate_and_process(){
       var argument_form = "<table id='arg_inp'><tr><th>Name of Variable</th><th>Type of Variable</th><th>Dimension of Variable</th></tr>";
       for(var i=0; i<result;i++){
          argument_form += "<tr><td><input type='text' size='20' name='arg_name_"+i+"'></td>";
-         argument_form += "<td><input type='text' size='20' name='arg_type_"+i+"'></td>";
-         argument_form += "<td><input type='text' size='20'  name='arg_dim_"+i+"'></td>";
+         argument_form += "<td align='center'><select name='arg_type_"+i+"'><option value='int'>int</option></select></td>";
+         argument_form += "<td align='center'><select name='arg_dim_"+i+"'><option value='0'>0</option><option value='1'>1</option><option value='2'>2</option></select></td>";
          argument_form += "</tr>"
       }
       if(result!=0)
